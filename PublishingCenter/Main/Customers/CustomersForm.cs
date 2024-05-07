34f776e0baa90f5275535326bc65811506c69138
Polina Kosova <kosowa.polina1@yandex.ru>
2024-05-07 22:31:30 +0500

    added customers form and customer card form

--- PublishingCenter/Main/Customers/CustomersForm.cs
+++ PublishingCenter/Main/Customers/CustomersForm.cs
@@ -1,5 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using ServiceStack;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,47 +9,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PublishingCenter
+namespace PublishingCenter.Main.Customers
 {
-    public partial class AuthorsForm : Form
+    public partial class CustomersForm : Form
     {
         private MySqlConnection connection;
-        public AuthorsForm()
+        public CustomersForm()
         {
             InitializeComponent();
             connection = new Connection().GetConnectionString();
 
-            dataGridViewAuthors.Width = Width;
-            buttonAdd.Location = new System.Drawing.Point(dataGridViewAuthors.Width + 130, dataGridViewAuthors.Location.Y);
+            dataGridViewCustomers.Width = Width;
+            buttonAdd.Location = new System.Drawing.Point(dataGridViewCustomers.Width + 130, dataGridViewCustomers.Location.Y);
         }
 
-        private void AuthorsForm_Load(object sender, EventArgs e)
+        private void CustomersForm_Load(object sender, EventArgs e)
         {
             UpdateTable();
         }
 
         private void UpdateTable()
         {
-            dataGridViewAuthors.Rows.Clear();
+            dataGridViewCustomers.Rows.Clear();
 
             try
             {
                 if (connection.State == ConnectionState.Closed)
                     connection.Open();
-                string query = "SELECT * FROM Authors";
+                string query = "SELECT * FROM Customers";
                 MySqlCommand command = new MySqlCommand(query, connection);
                 MySqlDataAdapter adapter = new MySqlDataAdapter(command);
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
                 foreach (DataRow row in dataTable.Rows)
                 {
-                    dataGridViewAuthors.Rows.Add(row.ItemArray[0], row.ItemArray[2], row.ItemArray[1], row.ItemArray[3], ((DateTime)row.ItemArray[4]).ToShortDateString());
+                    dataGridViewCustomers.Rows.Add(row.ItemArray[0], row.ItemArray[1], row.ItemArray[2], row.ItemArray[3], row.ItemArray[4]);
                 }
-
-
-                //    System.Data.DataSet dataSet = new System.Data.DataSet();
-                //    adapter.Fill(dataSet, "Authors");
-                //    dataGridViewAuthors.DataSource = dataSet.Tables["Authors"];
             }
             catch (Exception ex)
             {
@@ -65,25 +59,21 @@ namespace PublishingCenter
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-            AuthorCardForm authorCardForm = new AuthorCardForm(true);
-            authorCardForm.ShowDialog();
+            CustomerCardForm customerCardForm = new CustomerCardForm(true);
+            customerCardForm.ShowDialog();
 
             UpdateTable();
         }
 
-        private void dataGridViewAuthors_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridViewCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.ColumnIndex == 1 && e.RowIndex >= 0)
             {
                 try
                 {
-                    //connection.Open();
-                    string id = dataGridViewAuthors.Rows[e.RowIndex].Cells[0].Value.ToString();
-                    //string query = "SELECT * FROM Authors WHERE id = @Id LIMIT 1";
-                    //MySqlCommand command = new MySqlCommand(query, connection);
-                    //command.Parameters.AddWithValue("@AuthorId", id);
-                    AuthorCardForm authorCardForm = new AuthorCardForm(false, id);
-                    authorCardForm.ShowDialog();
+                    string id = dataGridViewCustomers.Rows[e.RowIndex].Cells[0].Value.ToString();
+                    CustomerCardForm customerCardForm = new CustomerCardForm(false, id);
+                    customerCardForm.ShowDialog();
 
                 }
                 catch (Exception ex)
